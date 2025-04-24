093d97ddc008ec081918b26091056f61be588673
HungHa <hungtmt1102@gmail.com>
2025-04-24 14:09:56 +0700

    Fix tParams inProduct Detail

--- app/(app)/products/[slug]/page.tsx
+++ app/(app)/products/[slug]/page.tsx
@@ -1,16 +1,15 @@
 import { getProductBySlug } from "@/app/lib/data";
 import ProductDetail from "@/app/ui/app/products/product-detail";
 
-interface ProductDetailProps {
-  params: { slug: string };
-}
+type tParams = Promise<{ slug: string }>;
 
-export default async function Page({ params }: ProductDetailProps) {
-  const product = await getProductBySlug(params.slug);
+export default async function Page({ params }: { params: tParams }) {
+  const { slug }: { slug: string } = await params;
+  const product = await getProductBySlug(slug);
   // console.log(products);
   return (
     <div className="container mx-auto px-4 py-10 text-[rgb(121,100,73)]">
-      <ProductDetail product={product} />
+      <ProductDetail product={product!} />
     </div>
   );
 }
