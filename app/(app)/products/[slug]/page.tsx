de0cf189ce4e09ebec8671ab9e7f5db2a5469492
HungHa <hungtmt1102@gmail.com>
2025-04-24 14:28:23 +0700

    Fix unidecode

--- app/(app)/products/[slug]/page.tsx
+++ app/(app)/products/[slug]/page.tsx
@@ -6,7 +6,6 @@ type tParams = Promise<{ slug: string }>;
 export default async function Page({ params }: { params: tParams }) {
   const { slug }: { slug: string } = await params;
   const product = await getProductBySlug(slug);
-  // console.log(products);
   return (
     <div className="container mx-auto px-4 py-10 text-[rgb(121,100,73)]">
       <ProductDetail product={product!} />
---
+++ unidecode.d.ts
@@ -0,0 +1,4 @@
+declare module 'unidecode' {
+  const unidecode: (input: string) => string;
+  export = unidecode;
+}
