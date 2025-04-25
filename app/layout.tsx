01c66244e5663fec5050fa77db7af580e86b1e01
HungHa <hungtmt1102@gmail.com>
2025-04-25 11:20:02 +0700

    fix viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0, user-scalable=yes" />
       </head>
       <body className={`${lusitana} antialiased`}>
         {/* <Providers> */}
