b6b55617f923b023b35e7e89d6d238234c94c35c
HungHa <hungtmt1102@gmail.com>
2025-05-09 22:58:34 +0700

    Add loading page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,9 @@ import HomeContent from "./home";
 import { Providers } from "./providers";
 import { Metadata } from 'next';
 
-
-
 export const metadata: Metadata = {
   title: 'Trang chủ',
 };
-
 
 export default async function ProductPage() {
   const lastestProducts = await getLastestProducts();
