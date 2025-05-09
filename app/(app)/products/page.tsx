2db65dfd7c536a077efed7216a9a96049f6a8fe0
HungHa <hungtmt1102@gmail.com>
2025-05-09 23:20:30 +0700

    Fix

--- app/(app)/products/page.tsx
+++ app/(app)/products/page.tsx
@@ -1,6 +1,6 @@
 import AllProducts from "@/app/ui/app/products/allProducts";
 import { Metadata } from 'next';
-
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: 'Tất cả sản phẩm',
